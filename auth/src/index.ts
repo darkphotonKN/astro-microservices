fd9757ec74c81c03eeaceea2e171f5843dbebb83
Kranti <darkphoton20@gmail.com>
2022-04-06 17:10:48 +0800

    Adding mongodb in auth service k8s setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentRouterUser } from './routes/current-user';
 
@@ -18,13 +20,26 @@ app.use(signUpRouter);
 app.use(signOutRouter);
 
 // catch any undefined routes and throw custom NOT FOUND error
-app.all('*', () => {
+app.all('*', async () => {
   throw new NotFoundError();
 });
 
 // custom middleware to handle errors
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Auth service listening on port 3000!');
-});
+const start = async () => {
+  try {
+    // mongodb uri format: mongodb://domain(use name of k8s service):port(default 27017)/dbName(mongodb creates one for you if this db name wasn't already present)
+    await mongoose.connect('mongodb://auth-monogo-srv:27017/auth');
+    console.log('Connected to mongoDB');
+  } catch (err) {
+    console.log('Error while connection to mongoDB:', err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Auth service listening on port 3000!');
+  });
+};
+
+// initiate database setup, connection, and initializing server
+start();
